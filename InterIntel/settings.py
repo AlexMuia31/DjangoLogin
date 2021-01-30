e94bd6b7750452cd9d2345bb0ee63de0fd449c6b
AlexMuia31 <alexmuia31@gmail.com>
2021-01-30 12:32:51 +0300

    uwsgi

--- InterIntel/settings.py
+++ InterIntel/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'InterIntel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'inter',
+        'USER': 'django',
+        'PASSWORD': '145200',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
